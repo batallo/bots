92727a7eb33fabdd569818c7bcf1464c705c0492
anton shautsou <79911099+batallo@users.noreply.github.com>
2024-09-06 01:11:36 +0200

    Moo init (#2)
    * test ci
    
    * update naming
    
    * swap places
    
    * adjust logging
    
    * upd
    
    * upd zip
    
    * moo init
    
    ---------
    
    Co-authored-by: Anton Shautsou <anton.shautsou@wolterskluwer.com>

---
+++ moo_v_bot/index.ts
@@ -0,0 +1,52 @@
+import AWS from 'aws-sdk';
+import { MooVBot } from './src';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const mooVBot = new MooVBot(process.env.TOKEN_BOT_MOO_V as string);
+
+export async function handler(event: any) {
+  const request = event.body && JSON.parse(event.body);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!')
+  };
+
+  if (!request) return response;
+
+  const innerValue = request.message || request.channel_post;
+  // const memberAction = request.body.my_chat_member;
+
+  console.warn(innerValue);
+
+  if (innerValue) {
+    const chatId = innerValue.chat.id;
+    const inputMessage = innerValue.text;
+    if (inputMessage) {
+      if (inputMessage == 'getDb') {
+        let txt: any;
+
+        const params = {
+          TableName: 'moo_v_bot',
+          KeyConditionExpression: 'group_id = :gid',
+          ExpressionAttributeValues: {
+            ':gid': 0
+          }
+        };
+
+        try {
+          const data = await docClient.query(params).promise();
+          txt = data.Items;
+        } catch (err) {
+          console.error('Error', err);
+        }
+        await mooVBot.sendToTelegram(chatId, txt);
+      } else if (inputMessage == 'lol') {
+        await mooVBot.sendToTelegram(chatId, 'kek');
+      } else {
+        await mooVBot.sendToTelegram(chatId, mooVBot.getRythme(inputMessage));
+      }
+    }
+  }
+
+  return { statusCode: 200 };
+}
