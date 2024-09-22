bf49ef99e43458320a89347b3b0a100e6b1286f6
Anton Shautsou <anton.shautsou@wolterskluwer.com>
2024-09-22 15:35:49 +0200

    test workflow

--- common/bot_base.ts
+++ common/bot_base.ts
@@ -5,11 +5,13 @@ export class BaseBot {
   protected botName: string;
   protected dynamoDbClient: DynamoDbBase;
   private botToken: string;
+  private telegramUrl: string;
 
   constructor(botName: string, botToken: string) {
     this.botName = botName;
     this.botToken = botToken;
     this.dynamoDbClient = new DynamoDbBase(botName);
+    this.telegramUrl = `https://api.telegram.org/bot${this.botToken}`;
   }
 
   isCommand(input: string) {
@@ -24,7 +26,7 @@ export class BaseBot {
   async sendToTelegram(chatId: number, message: string, options?: TelegramSendParam) {
     const urlEnding = options?.updateMessageId ? 'editMessageText' : 'sendMessage';
     const response = await axios
-      .post(`https://api.telegram.org/bot${this.botToken}/${urlEnding}`, {
+      .post(`${this.telegramUrl}/${urlEnding}`, {
         chat_id: chatId,
         message_id: options?.updateMessageId,
         text: message,
@@ -41,7 +43,7 @@ export class BaseBot {
 
   async sendToTelegramPoll(chatId: number, pollQuestion: string, pollOptions: string[], settings?: TelegramPollSettings) {
     const response = await axios
-      .post(`https://api.telegram.org/bot${this.botToken}/sendPoll`, {
+      .post(`${this.telegramUrl}/sendPoll`, {
         chat_id: chatId,
         question: pollQuestion,
         options: pollOptions,
@@ -53,6 +55,26 @@ export class BaseBot {
         throw new Error(err);
       });
     return response?.data;
+  }
+
+  private async getTelegramGroupAdmins(chatId: number) {
+    const response = await axios
+      .post(`${this.telegramUrl}/getChatAdministrators`, {
+        chat_id: chatId
+      })
+      .catch(err => {
+        const errorNotice = '-=ERROR ********** ERROR=-';
+        console.error(errorNotice + '\n' + err.response.data + '\n' + errorNotice);
+        throw new Error(err);
+      });
+    return response?.data;
+  }
+
+  async isUserAdmin(chatId: number, userId: number) {
+    const admins = await this.getTelegramGroupAdmins(chatId);
+    if (!admins?.ok) return await this.sendToTelegram(chatId, 'I cannot start the vote until I would be granted an admin role');
+    const isAdmin = admins?.result?.some((admin: Record<'user', Record<'id', number>>) => admin.user.id == userId) ?? false;
+    return isAdmin;
   }
 
   getRythme(input: string) {
